446c45e8b95b4e8e993e0136b1651ed37ea00e7b
rothstein99 <rothstein99@yahoo.com>
2016-03-26 15:59:33 -0400

    BRBS - Added new test.  Made SetUp method for test fixture.

--- src/movierental/CustomerStatementTest.java
+++ src/movierental/CustomerStatementTest.java
@@ -3,26 +3,55 @@ package movierental;
 import static org.junit.Assert.*;
 import junit.framework.Assert;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class CustomerStatementTest {
 
-	@Test
-	public void test() {
-		Customer customer1 = new Customer("Liver Lips");
-		Movie movie1 = MovieFactory.create("Deadpool", Movie.NEW_RELEASE);
-		Movie movie2 = MovieFactory.create("Tron", Movie.CHILDRENS);
-		Movie movie3 = MovieFactory.create("Star Wars", Movie.REGULAR);
-		Rental rental1 = new Rental(movie1, 5);
-		Rental rental2 = new Rental(movie2, 4);
-		Rental rental3 = new Rental(movie3, 3);
-		
-		customer1.addRental(rental1);
-		customer1.addRental(rental2);
-		customer1.addRental(rental3);
-		
-		String statement1 = customer1.statement();
-		assertEquals("Rental Record for Liver Lips\n\tDeadpool\t15.0\n\tTron\t3.0\n\tStar Wars\t3.5\nAmount owed is 21.5\nYou earned 4 frequent renter points", statement1);
-	}
+	Customer customer;
+	Movie deadPoolMovie;
+	Movie tronMovie;
+	Movie starWarsMovie;
 
+	@Before
+	public void init(){
+		customer = new Customer("Liver Lips");
+		deadPoolMovie = MovieFactory.create("Deadpool", Movie.NEW_RELEASE);
+		tronMovie = MovieFactory.create("Tron", Movie.CHILDRENS);
+		starWarsMovie = MovieFactory.create("Star Wars", Movie.REGULAR);
+	}
+	
+	@Test
+	public void getStatementForLiverLipsWithThreeRentals() {
+		Customer liverLips = new Customer("Liver Lips");
+		liverLips.addRental(new Rental(deadPoolMovie, 5));
+		liverLips.addRental(new Rental(tronMovie, 4));
+		liverLips.addRental(new Rental(starWarsMovie, 3));
+		
+		String expectedStatement = 
+				"Rental Record for Liver Lips\n"
+				+ "\tDeadpool\t15.0\n"
+				+ "\tTron\t3.0\n"
+				+ "\tStar Wars\t3.5\n"
+				+ "Amount owed is 21.5\n"
+				+ "You earned 4 frequent renter points";
+		assertEquals(expectedStatement, liverLips.statement());
+	}
+	
+	@Test
+	public void getAllRentalsForOneDayEach() {
+		Customer liverLips = new Customer("Liver Lips");
+		liverLips.addRental(new Rental(deadPoolMovie, 1));
+		liverLips.addRental(new Rental(tronMovie, 1));
+		liverLips.addRental(new Rental(starWarsMovie, 1));
+		
+		String expectedStatement = 
+				"Rental Record for Liver Lips\n"
+				+ "\tDeadpool\t3.0\n"
+				+ "\tTron\t1.5\n"
+				+ "\tStar Wars\t2.0\n"
+				+ "Amount owed is 6.5\n"
+				+ "You earned 3 frequent renter points";
+		assertEquals(expectedStatement, liverLips.statement());
+	}
 }
