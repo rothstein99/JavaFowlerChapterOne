7cac5094ac0b7af29664a5047bdf51d8f7c595c0
rothstein99 <rothstein99@yahoo.com>
2016-04-10 15:45:11 -0400

    BRBS - Removing bin files

--- src/movierental/StatementInfo.java
+++
@@ -1,9 +0,0 @@
-package movierental;
-
-public class StatementInfo {
-  public Movie movie;
-  public double totalAmount =0;
-  public int frequentRenterPoints = 0;
-  public double thisAmount = 0;
-
-}
