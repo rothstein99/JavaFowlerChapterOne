3eeb7a903ba191153971eea28e7c82945387cd77
rothstein99 <rothstein99@yahoo.com>
2016-06-27 20:30:32 -0400

    BSBR - Extracted out the switch stamement to calculate the amount owed as well as the frequent renter points.

--- src/movierental/Customer.java
+++ src/movierental/Customer.java
@@ -25,24 +25,7 @@ public class Customer {
         String result = "Rental Record for " + getName() + "\n";
 
         for (Rental each: _rentals) {
-            double thisAmount = 0;
-
-            //determine amounts for each line
-            switch (each.getMovie().getPriceCode()) {
-                case Movie.REGULAR:
-                    thisAmount += 2;
-                    if (each.getDaysRented() > 2)
-                        thisAmount += (each.getDaysRented() - 2) * 1.5;
-                    break;
-                case Movie.NEW_RELEASE:
-                    thisAmount += each.getDaysRented() * 3;
-                    break;
-                case Movie.CHILDRENS:
-                    thisAmount += 1.5;
-                    if (each.getDaysRented() > 3)
-                        thisAmount += (each.getDaysRented() - 3) * 1.5;
-                    break;
-            }
+            double thisAmount = amountFor(each);
 
             // add frequent renter points
             frequentRenterPoints++;
@@ -61,4 +44,24 @@ public class Customer {
 
         return result;
     }
+
+	private double amountFor(Rental each) {
+		double thisAmount = 0;
+		switch (each.getMovie().getPriceCode()) {
+		    case Movie.REGULAR:
+		        thisAmount += 2;
+		        if (each.getDaysRented() > 2)
+		            thisAmount += (each.getDaysRented() - 2) * 1.5;
+		        break;
+		    case Movie.NEW_RELEASE:
+		        thisAmount += each.getDaysRented() * 3;
+		        break;
+		    case Movie.CHILDRENS:
+		        thisAmount += 1.5;
+		        if (each.getDaysRented() > 3)
+		            thisAmount += (each.getDaysRented() - 3) * 1.5;
+		        break;
+		}
+		return thisAmount;
+	}
 }
