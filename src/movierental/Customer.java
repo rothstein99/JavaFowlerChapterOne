1f8aec5ea7e3c5ad31923aee31f09bad1933db2f
rothstein99 <rothstein99@yahoo.com>
2016-06-27 21:33:54 -0400

    BRBS - Replace temp thisAmount variable with the query.

--- src/movierental/Customer.java
+++ src/movierental/Customer.java
@@ -25,8 +25,6 @@ public class Customer {
         String result = "Rental Record for " + getName() + "\n";
 
         for (Rental each: _rentals) {
-            double thisAmount = each.getCharge();
-
             // add frequent renter points
             frequentRenterPoints++;
             // add bonus for a two day new release rental
@@ -34,8 +32,8 @@ public class Customer {
                 frequentRenterPoints++;
 
             // show figures for this rental
-            result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(thisAmount) + "\n";
-            totalAmount += thisAmount;
+            result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(each.getCharge()) + "\n";
+            totalAmount += each.getCharge();
         }
 
         // add footer lines
